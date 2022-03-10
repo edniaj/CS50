b005c224ff7738f84f2cfa9f168ca7d91d9ad8f5
jiande <jiande_goh@mymail.sutd.edu.sg>
2022-03-10 23:42:51 +0000

    Thu, Mar 10, 2022, 11:42 PM +00:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -1,20 +1,27 @@
 #include <cs50.h>
 #include <stdio.h>
 
-void leftTower(void);
+void leftTower(int height);
 int main(void)
 {
     int height;
     do
     {
         height = get_int("Height: ");
-    } while (height < 1 || height > 8)
+    } while (height < 1 || height > 8);
 }
 
 void leftTower(int height)
 {
-    for (int i = height; i > 0; i--)
+    for (int i = height-1; i >= 0; i--)
     {
-        for(int j=)
+        for(int j=i; j>0; j-- )
+        {
+            printf('.');
+        }
+        printf('#');
     }
+
 }
+
+leftTower(3)
