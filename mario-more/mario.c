92aa4234bdced8f356653a811936019b2867667e
jiande <jiande_goh@mymail.sutd.edu.sg>
2022-03-10 23:35:50 +0000

    Thu, Mar 10, 2022, 11:35 PM +00:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -1,7 +1,20 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(void) 
+void leftTower(void);
+int main(void)
 {
-    
+    int height;
+    do
+    {
+        height = get_int("Height: ");
+    } while (height < 1 || height > 8)
+}
+
+void leftTower(int height)
+{
+    for (int i = 0; i < height; i++)
+    {
+        
+    }
 }
