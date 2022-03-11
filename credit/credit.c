c48ad7aaec5ab3e3efa0b9971a7d1af4ae50bd22
jiande <jiande_goh@mymail.sutd.edu.sg>
2022-03-11 00:16:55 +0000

    Fri, Mar 11, 2022, 12:16 AM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -1,12 +1,13 @@
 #include <cs50.h>
 #include <stdio.h>
-
+#include <string.h>
 int main(void)
 {
     string creditCard = get_string("Number: ");
     printf("%s", creditCard);
-    for (int i = 0; i < strlen(creditCard); i++)
+    int length = strlen(creditCard);
+    for (int i = 0; i < length; i++)
     {
-        typeid()
+        printf("%s", typeid(cardCard[i]));
     }
 }
