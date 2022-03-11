d0e7f20345243f9e742b086b88a836213df3c149
jiande <jiande_goh@mymail.sutd.edu.sg>
2022-03-11 21:50:10 +0000

    Fri, Mar 11, 2022, 9:50 PM +00:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -1,7 +1,25 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <ctype.h>
+
+int convertKey(string)
+
 
 int main(int argc, string argv[])
 {
-    fd
+    // Convert key into index. Check for non-integer arvv[2]
+
+    // argv[1] is word to censor.
+
+    // Tranlate every word into the next word. Check for overflows. use islower() for different casing
+}
+
+int convertKey(string keyInString) {
+    int lengthKey = strlen(keyInString);
+
+    for(int i =0; i< lengthKey; i++ )
+    {
+        // Check for each character
+        if (keyInString[i])
+    }
 }
