34c7f15dc5d3916421b397ab54ddb6a92f29dac2
jiande <jiande_goh@mymail.sutd.edu.sg>
2022-03-11 00:36:41 +0000

    Fri, Mar 11, 2022, 12:36 AM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -4,12 +4,16 @@
 int main(void)
 {
     string creditCard = get_string("Number: ");
+    int total =0;
     unsigned long lengthOfCard = strlen(creditCard);
     if (( lengthOfCard != 13 && lengthOfCard !=15 ) && lengthOfCard != 16 ) printf("gg %lu",lengthOfCard);
 
 
-    for(int i=0; i< lengthOfCard; i++)
+    for(int i=lengthOfCard-2; i>0; i++)
     {
-        
+        int temp = (int) creditCard[i] * 2;
+        if (temp >10) {
+            
+        }
     }
 }
