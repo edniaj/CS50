6343850617d97994c48fd52364db767cdf8041e1
jiande <jiande_goh@mymail.sutd.edu.sg>
2022-03-11 21:56:40 +0000

    Fri, Mar 11, 2022, 9:56 PM +00:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -4,22 +4,33 @@
 
 int convertKey(string)
 
-
 int main(int argc, string argv[])
 {
     // Convert key into index. Check for non-integer arvv[2]
+    const int keyInString = get_int("");
+    const int key = convertKey(argv[2]);
+
+    string plainText = argv[1];
 
     // argv[1] is word to censor.
 
     // Tranlate every word into the next word. Check for overflows. use islower() for different casing
 }
 
-int convertKey(string keyInString) {
+int convertKey(string keyInString)
+{
     int lengthKey = strlen(keyInString);
-
-    for(int i =0; i< lengthKey; i++ )
+    for (int i = 0; i < lengthKey; i++)
     {
         // Check for each character
-        if (keyInString[i])
+        if (isdigit(keyInString[i]) != 0)
+        {
+            continue
+        }
+        else
+        {
+            return 0;
+        }
     }
+    return atoi(keyInString) // Handle testcase where key is invalid later
 }
