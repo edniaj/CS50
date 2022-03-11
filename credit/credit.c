75776e57a21cdc1c72c1b06cd6c7875dd547bbcf
jiande <jiande_goh@mymail.sutd.edu.sg>
2022-03-11 03:15:39 +0000

    Fri, Mar 11, 2022, 3:15 AM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -9,46 +9,46 @@ int main(void)
     int total = 0;
     unsigned long lengthOfCard = strlen(creditCard);
 
-    char firstChar = creditCard[0];
-    char secondChar = creditCard[1];
-    printf("%c",strcmp(&firstChar ,"3") ? 'true': 'false');
-    // for (int i = lengthOfCard - 2; i >= 0; i = i - 2)
-    // {
-    //     int temp = ((int)creditCard[i] - 48) * 2; //
-    //     if (temp > 10)
-    //     {
-    //         total++; // Max total is 18
-    //         temp -= 10;
-    //     }
-    //     total += temp;
-    // }
-    // for (int i = lengthOfCard - 1; i >= 0; i = i - 2)
-    // {
-    //     int temp = ((int)creditCard[i] - 48);
-    //     total += temp;
-    // }
-    // if (total % 10 == 0)
-    // {
-    //     if ( ( lengthOfCard == 13 || lengthOfCard == 16)&& strcmp(firstChar,"4"))
-    //     {
-    //         printf("VISA");
-    //     }
+    int firstChar = (int) creditCard[0] - 48;
+    int secondChar = (int) creditCard[1] - 48;
 
-    //     if (lengthOfCard == 15 &&  strcmp(firstChar ,"3") &&  ( firstChar ,"4") || secondChar ,"7")  ))
-    //         {
-    //             printf("American Express");
-    //         }
-    //     if (lengthOfCard == 16)
-    //     {
-    //         if (    strcmp(firstChar,"5")   &&  ( strcmp(secondChar ,"1") || strcmp(secondChar ,"2") || strcmp(secondChar ,"3") || strcmp(secondChar ,"4") || strcmp(secondChar ,"5") ) )
-    //         {
-    //             printf("\nchar is : %c\n", firstChar ); //4003600000000014
-    //             printf("MasterCard");
-    //         }
-    //     }
-    // }
-    // else
-    // {
-    //     printf("INVALID");
-    // }
+    for (int i = lengthOfCard - 2; i >= 0; i = i - 2)
+    {
+        int temp = ((int)creditCard[i] - 48) * 2; //
+        if (temp > 10)
+        {
+            total++; // Max total is 18
+            temp -= 10;
+        }
+        total += temp;
+    }
+    for (int i = lengthOfCard - 1; i >= 0; i = i - 2)
+    {
+        int temp = ((int)creditCard[i] - 48);
+        total += temp;
+    }
+    if (total % 10 == 0)
+    {
+        if ( ( lengthOfCard == 13 || lengthOfCard == 16)&& strcmp(firstChar,"4"))
+        {
+            printf("VISA");
+        }
+
+        if (lengthOfCard == 15 &&  strcmp(firstChar ,"3") &&  ( firstChar ,"4") || secondChar ,"7")  ))
+            {
+                printf("American Express");
+            }
+        if (lengthOfCard == 16)
+        {
+            if (    strcmp(firstChar,"5")   &&  ( strcmp(secondChar ,"1") || strcmp(secondChar ,"2") || strcmp(secondChar ,"3") || strcmp(secondChar ,"4") || strcmp(secondChar ,"5") ) )
+            {
+                printf("\nchar is : %c\n", firstChar ); //4003600000000014
+                printf("MasterCard");
+            }
+        }
+    }
+    else
+    {
+        printf("INVALID");
+    }
 }
