b2aee8d6a99e3a1cbd5cdc9af5f2b92724bbb1ff
jiande <jiande_goh@mymail.sutd.edu.sg>
2022-03-12 01:04:26 +0000

    Sat, Mar 12, 2022, 1:04 AM +00:00

--- week 2/substitution/substitution.c
+++ week 2/substitution/substitution.c
@@ -1,7 +1,34 @@
 #include <cs50.h>
 #include <stdio.h>
-
-int main(int argc, string argv[]) 
+#include <string.h>
+#include <ctype.h>
+int main(int argc, string argv[])
 {
+    // ensure argc is 26
+    if (argc != 26)
+    {
+        printf("Key must contain 26 characters.");
+        return 1;
+    }
+    string plainText = get_string("plaintext: ");
+    const int stringLength = strlen(argv[1]);
 
+    for (int i = 0; i < stringLength; i++)
+    {
+        if (isupper(plainText[i]))
+        {
+            int indexChar = (int)plainText[i] - 'A';
+            plainText[i] = toupper(argv[1][indexChar]);
+        }
+        if (islower(plainText[i]))
+        {
+            int indexChar = (int)plainText[i] - 'a';
+            plainText[i] = tolower(argv[1][indexChar]);
+        }
+    }
+    printf("%s", plainText);
+
+    // Use an if statement to convert back to upper / lowercase
+
+    // convert plaintext to cipher text
 }
