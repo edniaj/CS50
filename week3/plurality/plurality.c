fab20da951796c35451cf8c589614453ec462374
jiande <jiande_goh@mymail.sutd.edu.sg>
2022-03-12 15:58:20 +0000

    Sat, Mar 12, 2022, 3:58 PM +00:00

--- week3/plurality/plurality.c
+++ week3/plurality/plurality.c
@@ -81,6 +81,7 @@ bool vote(string name)
 // Print the winner (or winners) of the election
 void print_winner(void)
 {
+    candidate newList[candidate_count];
     for (int i = 0; i < candidate_count; i++)
     {
         // Selection sort
