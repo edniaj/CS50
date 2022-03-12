90eaa2734f1361d02eeb8957287f356ef12fb330
jiande <jiande_goh@mymail.sutd.edu.sg>
2022-03-12 00:23:55 +0000

    Sat, Mar 12, 2022, 12:23 AM +00:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -13,51 +13,51 @@ int main(int argc, string argv[])
     if (argc > 3) {
         printf("Usage: ./caesar key");
     }
-    // const int keyInString = get_int("");
-    // const int key = convertKey(argv[2]);
+    const int keyInString = get_int("");
+    const int key = convertKey(argv[2]);
 
-    // string plainText = argv[1];
-    // int lengthText = strlen(plainText);
+    string plainText = argv[1];
+    int lengthText = strlen(plainText);
 
-    // for (int i = 0; i < lengthText; i++)
-    // {
-    //     int index;
-    //     if (isupper(plainText[i]))
-    //     {
-    //         index = (int)plainText[i] - 'A' + 1;
-    //         index += key;
-    //         index %= 26;
-    //         index += 'A';
-    //         plainText[i] = (char)index;
-    //     }
-    //     if (islower(plainText[i]))
-    //     {
-    //         index = (int)plainText[i] - 'a' + 1;
-    //         index += key;
-    //         index %= 26;
-    //         index += 'a';
-    //         plainText[i] = (char)index;
-    //     }
-    // }
-    // printf("%s", plainText);
+    for (int i = 0; i < lengthText; i++)
+    {
+        int index;
+        if (isupper(plainText[i]))
+        {
+            index = (int)plainText[i] - 'A' + 1;
+            index += key;
+            index %= 26;
+            index += 'A';
+            plainText[i] = (char)index;
+        }
+        if (islower(plainText[i]))
+        {
+            index = (int)plainText[i] - 'a' + 1;
+            index += key;
+            index %= 26;
+            index += 'a';
+            plainText[i] = (char)index;
+        }
+    }
+    printf("%s", plainText);
 }
 
-// int convertKey(string keyInString)
-// {
-//     int lengthKey = strlen(keyInString);
-//     for (int i = 0; i < lengthKey; i++)
-//     {
-//         // Check for each character
-//         if (isdigit(keyInString[i]) != 0)
-//         {
-//             continue;
-//         }
-//         else
-//         {
-//             return 0;
-//         }
-//     }
-//     int key = atoi(keyInString);
-//     key %= 26;
-//     return key ;// Handle testcase where key is invalid later
-// }
+int convertKey(string keyInString)
+{
+    int lengthKey = strlen(keyInString);
+    for (int i = 0; i < lengthKey; i++)
+    {
+        // Check for each character
+        if (isdigit(keyInString[i]) != 0)
+        {
+            continue;
+        }
+        else
+        {
+            return 0;
+        }
+    }
+    int key = atoi(keyInString);
+    key %= 26;
+    return key ;// Handle testcase where key is invalid later
+}
