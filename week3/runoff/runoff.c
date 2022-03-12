b78a8a8e8aaf1001ff3e6873da7bc4faa38dd791
jiande <jiande_goh@mymail.sutd.edu.sg>
2022-03-12 20:41:23 +0000

    Sat, Mar 12, 2022, 8:41 PM +00:00

--- week3/runoff/runoff.c
+++ week3/runoff/runoff.c
@@ -181,7 +181,7 @@ bool print_winner(void)
     for (int i = 0; i < candidate_count; i++)
     {
         printf("%i : %s\n",candidates[i].votes, candidates[i].name );
-        if (candidates[i].votes > minimumVote)
+        if (candidates[i].votes >= minimumVote)
         {
             printf("%s", candidates[i].name);
             return true;
