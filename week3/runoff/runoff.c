e7ab0d61c3d4af8c97a2aa32b43079898927554d
jiande <jiande_goh@mymail.sutd.edu.sg>
2022-03-12 19:26:58 +0000

    Sat, Mar 12, 2022, 7:26 PM +00:00

--- week3/runoff/runoff.c
+++ week3/runoff/runoff.c
@@ -168,6 +168,7 @@ void tabulate(void)
 // Print the winner of the election, if there is one
 bool print_winner(void)
 {
+    int winnerIndex =0;
     for (int i = 0; i < candidate_count; i++)
     {
         
