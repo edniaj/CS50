4c77d5eb4136e6cfaea45574627b29c7ab220a49
jiande <jiande_goh@mymail.sutd.edu.sg>
2022-03-13 22:06:13 +0000

    Sun, Mar 13, 2022, 10:06 PM +00:00

--- week4/volume/volume.c
+++ week4/volume/volume.c
@@ -46,10 +46,10 @@ int main(int argc, char *argv[])
     //Unsure how much data to allocate
     int16_t placeHolder;
 
-    // while(fread(&placeHolder, 2, 1, input))
-    // {
-    //     printf()
-    // }
+    while(fread(&placeHolder, 2, 1, input))
+    {
+        printf('a');
+    }
 
     while(fread(&placeHolder, 2, 1, input))
     {
