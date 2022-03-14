daf07d2bdeda4c7fdce5c37501f4b8873fe559c1
jiande <jiande_goh@mymail.sutd.edu.sg>
2022-03-14 00:39:20 +0000

    Mon, Mar 14, 2022, 12:39 AM +00:00

--- week4/filter-less/helpers.c
+++ week4/filter-less/helpers.c
@@ -74,7 +74,7 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
 // Blur image
 void blur(int height, int width, RGBTRIPLE image[height][width])
 {
-
+    RGBTRIPLE clone = 
     for (int i = 0; i < height; i++)
     {
         for (int j = 0; j < width; j++)
