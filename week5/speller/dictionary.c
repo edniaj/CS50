b2da833389b64e306f49cc814e5f8235fa0eb9db
jiande <jiande_goh@mymail.sutd.edu.sg>
2022-03-15 16:14:22 +0000

    Tue, Mar 15, 2022, 4:14 PM +00:00

--- week5/speller/dictionary.c
+++ week5/speller/dictionary.c
@@ -2,42 +2,53 @@
 
 #include <ctype.h>
 #include <stdbool.h>
-
+#include <stdlib.h>
+#include <stdio.h>
 #include "dictionary.h"
 
 // Represents a node in a hash table
 typedef struct node
-{
+    {
     char word[LENGTH + 1];
-    struct node *next;
-}
+    struct node* next;
+    }
 node;
 
 // TODO: Choose number of buckets in hash table
 const unsigned int N = 26;
 
 // Hash table
-node *table[N];
+node* table[N];
 
 // Returns true if word is in dictionary, else false
-bool check(const char *word)
-{
+bool check(const char* word)
+    {
     // TODO
     // traverse linked list, looking for the word (strcasecmp)
     return false;
-}
+    }
 
 // Hashes word to a number
-unsigned int hash(const char *word)
-{
+unsigned int hash(const char* word)
+    {
     // TODO: Improve this hash function
     return toupper(word[0]) - 'A';
-}
+    }
 
 // Loads dictionary into memory, returning true if successful, else false
-bool load(const char *dictionary)
-{
-    FILE *pRead = fread()
+bool load(const char* dictionary)
+    {
+    FILE* pReadFile = fread();
+    if (pReadFile == NULL)
+    {
+        printf("Lack of memory")
+        return false;
+    }
+    char* word[20];
+    while (fread(buffer, 20, 1,pReadFile) == 1)
+    {
+
+    }
     // TODO
     // Open dictionary file
         // Check if return value is NULL
@@ -52,18 +63,18 @@ bool load(const char *dictionary)
     // Insert node into hash table at that location
 
     return false; // If unable to open up a fiel
-}
+    }
 
 // Returns number of words in dictionary if loaded, else 0 if not yet loaded
 unsigned int size(void)
-{
+    {
     // TODO
     return 0;
-}
+    }
 
 // Unloads dictionary from memory, returning true if successful, else false
 bool unload(void)
-{
+    {
     // TODO
     return false;
-}
+    }
