5ee821f953bc53171fea737eaabfa0c007df5895
jiande <jiande_goh@mymail.sutd.edu.sg>
2022-03-15 00:54:30 +0000

    Tue, Mar 15, 2022, 12:54 AM +00:00

--- week4/recover/recover.c
+++ week4/recover/recover.c
@@ -16,8 +16,9 @@ int main(int argc, char *argv[]) // But you should ultimately find that the imag
     // }
     char* filename = NULL;
     sprintf(filename, "%03i.jpg", 2);
+    // 
 
-    // ###.jpg
+
     // Moreover, rather than read my memory card’s bytes one at a time, you can read 512 of them at a time into a buffer for efficiency’s sake
 
     // That is, you need only look for those signatures in a block’s first four bytes.
