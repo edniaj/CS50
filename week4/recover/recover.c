f85b34ed6067b965b4a90f313b5f6e28a9d7a88e
jiande <jiande_goh@mymail.sutd.edu.sg>
2022-03-15 00:26:28 +0000

    Tue, Mar 15, 2022, 12:26 AM +00:00

--- week4/recover/recover.c
+++ week4/recover/recover.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char *argv[])
+int main(int argc, char *argv[]) // But you should ultimately find that the image contains 50 JPEGs.
 {
+    if (argc != 2) return 1;
+
+
+    FILE *file = fopen(argv[1], "r");
+
     // Moreover, rather than read my memory card’s bytes one at a time, you can read 512 of them at a time into a buffer for efficiency’s sake
-    
+
     // That is, you need only look for those signatures in a block’s first four bytes.
 
-    //
+    // . But the last byte of a JPEG might not fall at the very end of a block.
 }
