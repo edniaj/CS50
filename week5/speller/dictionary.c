8e3f307fd481a268f54f49ccb089aaea2e803ced
jiande <jiande_goh@mymail.sutd.edu.sg>
2022-03-15 20:07:01 +0000

    Tue, Mar 15, 2022, 8:07 PM +00:00

--- week5/speller/dictionary.c
+++ week5/speller/dictionary.c
@@ -14,10 +14,9 @@ typedef struct node
 } node;
 
 // TODO: Choose number of buckets in hash table
-const unsigned int N = (LENGTH+1) * 'z';
+const unsigned int N = 40;
 
 // Hash table
-
 
 int totalWords = 0;
 node *table[N];
@@ -25,106 +24,81 @@ node *table[N];
 // Returns true if word is in dictionary, else false
 bool check(const char *word)
 {
-    int index  = hash(word);
+    int index = hash(word);
 
     node *cursor = table[index];
-    while(cursor!=NULL)
+    while (cursor != NULL)
     {
-        if(strcasecmp(cursor->, word) ==0)
+        if (strcasecmp(cursor->word, word) == 0)
         {
             return true;
         }
-        cursor = cursor -> next;
+        cursor = cursor->next;
     }
-    // TODO
-    // int hashIndex = hash(word);
-    // node *tmp = table[hashIndex];
-    // while (tmp != NULL)
-    // {
-    //     if (strcasecmp(tmp->word, word) == 0)
-    //     {
-    //         return true;
-    //     }
-    //     tmp = tmp->next;
-    // }
-    // return false;
+    return false;
 }
+// TODO
+// int hashIndex = hash(word);
+// node *tmp = table[hashIndex];
+// while (tmp != NULL)
+// {
+//     if (strcasecmp(tmp->word, word) == 0)
+//     {
+//         return true;
+//     }
+//     tmp = tmp->next;
+// }
+// return false;
+// }
 
 // Hashes word to a number
 unsigned int hash(const char *word)
 {
     // TODO: Improve this hash function
     int hashIndex = ((int)toupper(word[0]) * 4) % 40;
-    return toupper(word[0]);
+    return hashIndex;
+    // int sum = 0;
+    // for (int i = 0; i < strlen(word); i++)
+    // {
+    //     sum += tolower(word[i]);
+    // }
+    // return (sum % N);
 }
 
 // Loads dictionary into memory, returning true if successful, else false
 bool load(const char *dictionary)
 {
-    FILE *file = fopen(dictionary, "r");
-    if (file == NULL)
+
+    FILE *pRead = fopen(dictionary, "r");
+    if (pRead == NULL)
     {
+        printf("Memory ran out of space");
         return false;
     }
 
     char word[LENGTH + 1];
-    while (fscanf(file, "%s", word) != EOF)
+
+    while (fscanf(pRead, "%s", word) != EOF)
     {
-        node *new_node = malloc(sizeof(node));
-        if (new_node == NULL)
+        node *n = malloc(sizeof(node));
+        n->next = NULL; // We will insert node from the front
+        strcpy(n->word, word);
+        int hashIndex = hash(word);
+        if (table[hashIndex] == NULL)
         {
-            return false;
-        }
-
-        strcpy(new_node->word, word);
-        new_node->next = NULL;
-
-        int index = hash(word);
-        if (table[index] == NULL)
-        {
-            table[index] = new_node;
+            table[hashIndex] = n;
         }
         else
         {
-            new_node->next = table[index];
-            table[index] = new_node;
+            n->next = table[hashIndex];
+            table[hashIndex] = n;
         }
         totalWords++;
     }
-    fclose(file);
+    free(word);
+    fclose(pRead);
     return true;
 }
-// TODO
-// FILE *pRead = fopen(dictionary, "r");
-// if (pRead == NULL)
-// {
-//     printf("Memory ran out of space");
-//     return false;
-// }
-
-// char word[LENGTH + 1];
-
-// while (fscanf(pRead, "%s", word) != EOF)
-// {
-//     node *n = malloc(sizeof(node));
-//     n->next = NULL; // We will insert node from the front
-//     strcpy(n->word, word);
-//     int hashIndex = hash(word);
-//     if (table[hashIndex] == NULL)
-//     {
-//         table[hashIndex] = n;
-//     }
-//     else
-//     {
-//         n->next = table[hashIndex];
-//         table[hashIndex] = n;
-//     }
-//     totalWords++;
-// }
-// free(word);
-// fclose(pRead);
-// return true;
-// }
 
 // Returns number of words in dictionary if loaded, else 0 if not yet loaded
 unsigned int size(void)
@@ -137,37 +111,22 @@ bool unload(void)
 {
     for (int i = 0; i < N; i++)
     {
-        node *head = table[i];
-        node *cursor = head;
-        node *tmp = head;
-
-        while (cursor != NULL)
+        if (table[i] != NULL)
         {
-            cursor = cursor->next;
+            node *tmp = table[i]->next;
+            free(table[i]);
+            while (tmp != NULL)
+            {
+                node *tmp2 = tmp;
+                tmp = tmp->next;
+                free(tmp2);
+            }
             free(tmp);
-            tmp = cursor;
+        }
+        else
+        {
+            free(table[i]);
         }
     }
     return true;
 }
-    // for (int i = 0; i < N; i++)
-    // {
-    //     if (table[i] != NULL)
-    //     {
-    //         node *tmp = table[i]->next;
-    //         free(table[i]);
-    //         while (tmp != NULL)
-    //         {
-    //             node *tmp2 = tmp;
-    //             tmp = tmp->next;
-    //             free(tmp2);
-    //         }
-    //         free(tmp);
-    //     }
-    //     else
-    //     {
-    //         free(table[i]);
-    //     }
-    // }
-    // return true;
-// }
