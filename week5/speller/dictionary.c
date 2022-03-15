a497614c31f890b234b92c3e0258c5c185c20d47
jiande <jiande_goh@mymail.sutd.edu.sg>
2022-03-15 16:34:30 +0000

    Tue, Mar 15, 2022, 4:34 PM +00:00

--- week5/speller/dictionary.c
+++ week5/speller/dictionary.c
@@ -9,85 +9,82 @@
 
 // Represents a node in a hash table
 typedef struct node
-    {
+{
     char word[LENGTH + 1];
-    struct node* next;
-    }
-node;
+    struct node *next;
+} node;
 
 // TODO: Choose number of buckets in hash table
 const unsigned int N = 50;
 
 // Hash table
-node* table[N];
+node *table[N];
 
 // Returns true if word is in dictionary, else false
-bool check(const char* word)
-    {
+bool check(const char *word)
+{
     // TODO
     // traverse linked list, looking for the word (strcasecmp)
     return false;
-    }
+}
 
 // Hashes word to a number
-unsigned int hash(const char* word)
-    {
+unsigned int hash(const char *word)
+{
     // TODO: Improve this hash function
     return toupper(word[0]) - 'a' + strlen(word);
-    }
+}
 
 // Loads dictionary into memory, returning true if successful, else false
-bool load(const char* dictionary)
-    {
-    FILE* pReadFile = fread();
+bool load(const char *dictionary)
+{
+    FILE *pReadFile = fread();
     if (pReadFile == NULL)
-        {
-        printf("Lack of memory")
-            return false;
-        }
+    {
+        printf("Lack of memory") return false;
+    }
 
-    char* word[20];
+    char *word[20];
     while (fscanf(pReadFile, "%s", word) != NULL)
-        {
-        node* n = malloc(sizeof(node));
+    {
+        node *n = malloc(sizeof(node));
         n->word = strcpy(word);
         int hashIndex = hash(n->word);
         if (table[hashIndex] == NULL)
-            {// table[hashIndex] -> Head of pointer
+        { // table[hashIndex] -> Head of pointer
             table[hashIndex] = n
-            }
-        else
-            {
-
-            }
-
         }
+        else
+        {
+            n->next = table[hasIndex];
+        }
+    }
     // TODO
     // Open dictionary file
-        // Check if return value is NULL
+    // Check if return value is NULL
 
     // Read strings from file one at a time
-        // fscanf(file, "%s", word) , reads word until it returns EOF (end of file)
-    //Create a new node for each word
-        // Use malloc =>  if it returns NULL then fn return false
-        // strcpy into node using strcpy
+    // fscanf(file, "%s", word) , reads word until it returns EOF (end of file)
+    // Create a new node for each word
+    //  Use malloc =>  if it returns NULL then fn return false
+    //  strcpy into node using strcpy
     /// Hash word to obtain a hash value
-        // Generate our own hash function // all of them are islower()
+    // Generate our own hash function // all of them are islower()
     // Insert node into hash table at that location
 
     return false; // If unable to open up a fiel
-    }
+}
 
 // Returns number of words in dictionary if loaded, else 0 if not yet loaded
 unsigned int size(void)
-    {
+{
     // TODO
     return 0;
-    }
+}
 
 // Unloads dictionary from memory, returning true if successful, else false
 bool unload(void)
-    {
+{
     // TODO
     return false;
-    }
+}
