a793373fbbd655f807aed67d0582263ebdddced5
jiande <jiande_goh@mymail.sutd.edu.sg>
2022-03-15 06:37:06 +0000

    Tue, Mar 15, 2022, 6:37 AM +00:00

--- week4/recover/recover.c
+++ week4/recover/recover.c
@@ -20,7 +20,7 @@ int main(int argc, char *argv[]) // But you should ultimately find that the imag
     int writingData = 0;
     sprintf(fileName, "%0.3i.jpg", fileCount);
     FILE *pWriteFile = fopen(fileName, "w");
-    while (fread(buffer, 512, 1, pReadFile) == BLOCK_SIZE)
+    while (fread(buffer, 1, 512, pReadFile) == BLOCK_SIZE)
     {
         printf("%i",buffer[10]);
         if (checkBuffer(buffer) == 1)
