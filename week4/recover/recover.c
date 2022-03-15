daea7e04d6ea244055d7db025a17e6f4f86374fd
jiande <jiande_goh@mymail.sutd.edu.sg>
2022-03-15 00:24:32 +0000

    Tue, Mar 15, 2022, 12:24 AM +00:00

--- week4/recover/recover.c
+++ week4/recover/recover.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
- 
+
 int main(int argc, char *argv[])
 {
- 
+    // Moreover, rather than read my memory card’s bytes one at a time, you can read 512 of them at a time into a buffer for efficiency’s sake
+    
+    // That is, you need only look for those signatures in a block’s first four bytes.
+
+    //
 }
