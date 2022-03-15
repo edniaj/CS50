c0b2d9766721809e46cf84cf4fc0c11ac354260b
jiande <jiande_goh@mymail.sutd.edu.sg>
2022-03-15 13:44:10 +0000

    Tue, Mar 15, 2022, 1:44 PM +00:00

--- week4/inheritance/inheritance.c
+++ week4/inheritance/inheritance.c
@@ -40,7 +40,6 @@ int main(void)
 person *create_family(int generations)
 {
     // TODO: Allocate memory for new person
-
     // If there are still generations left to create
     if (generations > 1)
     {
