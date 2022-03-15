22ad0d34b7f6387a5b823fd13fe009cf632c6b31
jiande <jiande_goh@mymail.sutd.edu.sg>
2022-03-15 02:05:21 +0000

    Tue, Mar 15, 2022, 2:05 AM +00:00

--- week4/recover/recover.c
+++ week4/recover/recover.c
@@ -3,6 +3,7 @@
 #include <stdint.h>
 
 typedef uint8_t BYTE;
+void checkBuffer(BYTE bufferData);
 
 int main(int argc, char *argv[]) // But you should ultimately find that the image contains 50 JPEGs.
 {
@@ -12,21 +13,30 @@ int main(int argc, char *argv[]) // But you should ultimately find that the imag
     FILE *file = fopen(argv[1], "r");
 
     int BLOCK_SIZE = 512;
-    char* filename = malloc(20);
+    char* fileName = malloc(20);
     BYTE *buffer = malloc(sizeof(BYTE) * 512);
 
-    int fileCount =0
+    int fileCount =1;
+    int writingData = 0;
     while (fread(buffer, 1, BLOCK_SIZE, raw_file) == BLOCK_SIZE)
     {
+        if (checkBuffer(buffer) )
+        {
+            writingData =1;
+        }
+
+
         //Lets check it its a jpeg
             //If it is a jpeg, lets continue writing it until there is slack at the back
+            // Let's allow next loop to know we are still writing on a jpeg
+        // New file. No header data but we know its a jpeg since previous data has header data
 
         if(buffer[511] == 0)
         {
-            sprintf(filename, "%03i.jpg", fileCount);
             fileCount++;
-
+            sprintf(fileName, "%0.3i.jpeg", fileCount);
         }
+
     }
 
 
@@ -41,6 +51,17 @@ int main(int argc, char *argv[]) // But you should ultimately find that the imag
     // . But the last byte of a JPEG might not fall at the very end of a block.
 }
 
+void checkBuffer(BYTE bufferData)
+{
+    if (bufferData[0] == 0xff && bufferData[1] == 0xd8 && buffer[2] == 0xff && (bufferData[3] & 0xf0)==0xe0 )
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
 // rm -f *.jpg to remove all the jpg
 
 //check50 cs50/problems/2022/x/recover
