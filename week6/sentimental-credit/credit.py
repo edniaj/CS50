15f57fa384bfd11d3df19b22b0c145a2a484492b
jiande <jiande_goh@mymail.sutd.edu.sg>
2022-03-16 20:13:14 +0000

    Wed, Mar 16, 2022, 8:13 PM +00:00

--- week6/sentimental-credit/credit.py
+++ week6/sentimental-credit/credit.py
@@ -1,10 +1,32 @@
 from cs50 import get_int, get_string
 
 def main():
-    cardNumber = get_int("Number: ")
+    cardNumber = 0
+    while cardNumber <0:
+        try:
+            cardNumber = get_int("Number: ")
+        except:
+            cardNumber =-1
+            continue
     lengthCard = len(cardNumber)
     firstNumber = int(cardNumber[0])
     secondNumber = int(cardNumber[1])
+    total = 0
+    for(i in range(lengthCard-2,-1,-2)):
+        temp = 2 * int(cardNumber[i])
+        if temp > 10:
+            temp -=10
+            total +=1
+        total += temp
+    for(i in range(lengthCard-1,-1,-2)):
+        total += int(cardNumber[i])
 
-    for(i in range(lengthCard-2,-1,-2))
+    if total !=20:
+        print('INVALID\N')
+        return
+
+    
+
+
+
 main()
