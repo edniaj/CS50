1f41c529a6f2a5d823c185d03eaf5d00c33aba64
jiande <jiande_goh@mymail.sutd.edu.sg>
2022-03-16 20:05:18 +0000

    Wed, Mar 16, 2022, 8:05 PM +00:00

--- week6/sentimental-credit/credit.py
+++ week6/sentimental-credit/credit.py
@@ -1,6 +1,10 @@
 from cs50 import get_int, get_string
 
 def main():
-    cardNumber = get_int("Number: )
-                         
+    cardNumber = get_int("Number: ")
+    lengthCard = len(cardNumber)
+    firstNumber = int(cardNumber[0])
+    secondNumber = int(cardNumber[1])
+
+    for(i in range(lengthCard-2, ))
 main()
