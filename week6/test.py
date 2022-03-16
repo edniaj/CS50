ddc96541c7abf29a29646ebe9ce30e902cfe8036
jiande <jiande_goh@mymail.sutd.edu.sg>
2022-03-16 16:37:43 +0000

    Wed, Mar 16, 2022, 4:37 PM +00:00

--- week6/test.py
+++ week6/test.py
@@ -3,13 +3,16 @@ import sys
 
 
 
-with open("test.csv", "a") as file:
-    writer = csv.writer(file)
-    writer.writerow(sys.argv[1])
-
+houses = {
+    "Gryffindor": 0,
+    "Hufflepuff": 0,
+    "Ravenclaw": 0,
+    "Slytherin": 0
+}
 with open("hogwarts.csv", "r") as file:
     reader = csv.DictReader(file)
     for row in reader:
         house = row["House"]
         houses[house] +=1
 
+print(houses)
