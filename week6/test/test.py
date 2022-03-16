52bb7e301327bd2bbaabdd6c8921588cd523d9fa
jiande <jiande_goh@mymail.sutd.edu.sg>
2022-03-16 19:10:27 +0000

    Wed, Mar 16, 2022, 7:10 PM +00:00

--- week6/test/test.py
+++ week6/test/test.py
@@ -2,7 +2,6 @@ import csv
 import sys
 
 
-
 houses = {
     "Gryffindor": 0,
     "Hufflepuff": 0,
@@ -13,7 +12,7 @@ with open("hogwarts.csv", "r") as file:
     reader = csv.DictReader(file)
     for row in reader:
         house = row["House"]
-        houses[house] +=1
+        houses[house] += 1
 
 
 print(houses)
