42c641e0806a24a8a74575b09fe22de6bf00a888
jiande <jiande_goh@mymail.sutd.edu.sg>
2022-03-16 20:25:37 +0000

    Wed, Mar 16, 2022, 8:25 PM +00:00

--- week6/sentimental-credit/credit.py
+++ week6/sentimental-credit/credit.py
@@ -1,10 +1,12 @@
 from cs50 import get_int, get_string
 
 def main():
-    cardNumber = 0
-    while cardNumber <0:
+    cardNumber = -1
+    while int(cardNumber) <0:
         try:
-            cardNumber = get_int("Number: ")
+            cardNumber = get_string("Number: ")
+            if not cardNumber.isnumeric():
+                raise ValueError
         except:
             cardNumber =-1
             continue
@@ -12,20 +14,25 @@ def main():
     firstNumber = int(cardNumber[0])
     secondNumber = int(cardNumber[1])
     total = 0
-    for(i in range(lengthCard-2,-1,-2)):
+    for i in range(lengthCard-2,-1,-2) :
         temp = 2 * int(cardNumber[i])
         if temp > 10:
             temp -=10
             total +=1
         total += temp
-    for(i in range(lengthCard-1,-1,-2)):
+    for i in range(lengthCard-1,-1,-2) :
         total += int(cardNumber[i])
 
     if total % 10 ==0:
-        print('INVALID\N')
+        print('INVALID\n')
         return
 
-
+    if (len(cardNumber) in [13,16]) and cardNumber[0] == '4':
+        print('VISA\n')
+    if (len(cardNumber) == 15) and cardNumber[0] in ['34','37']:
+        print('AMEX\n')
+    if (len(cardNumber) == 16) and cardNumber[0] in ['51','52','53','54','55']:
+        print('MASTERCARD\n')
     # VISA = {
         # start with 4
     #     length: [13,16],
