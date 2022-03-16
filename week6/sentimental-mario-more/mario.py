4b8964c776190ff68ea100b4b37d9c6475347d2c
jiande <jiande_goh@mymail.sutd.edu.sg>
2022-03-16 19:57:49 +0000

    Wed, Mar 16, 2022, 7:57 PM +00:00

--- week6/sentimental-mario-more/mario.py
+++ week6/sentimental-mario-more/mario.py
@@ -1,3 +1,14 @@
 from cs50 import get_int
 
 
+def main():
+    height = get_int('Height: \n')
+    while(height < 1 or height > 8):
+        height = get_int('Height: \n')
+    for i in range(height):
+        gap = ' ' * (height - 1 - i)
+        tower = '#' * (i+1)
+        print(f'{gap}{tower} {tower}')
+
+main()
+
