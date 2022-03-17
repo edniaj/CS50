29bba5cdcd72bb9b162efc7e3c1ebf5b2038fe99
jiande <jiande_goh@mymail.sutd.edu.sg>
2022-03-17 05:22:45 +0000

    Thu, Mar 17, 2022, 5:22 AM +00:00

--- week2/readability/readability.c
+++ week2/readability/readability.c
@@ -3,6 +3,8 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
+#include <math.h>
+
 int main(void)
 {
     string word;
@@ -42,7 +44,7 @@ int main(void)
     printf("\n0.0588 * L = %f\n",t);
     t = 0.296 * S; // 1.24
     printf("\n - 0.296 * S  =%f",t);
-    int index = 0.0588 * L - 0.296 * S - 15.8; // 24.78 - 1.24 - 15.8 = 7.7 ~ 8
+    int index = round (0.0588 * L - 0.296 * S - 15.8 ); // 24.78 - 1.24 - 15.8 = 7.7 ~ 8
     printf("\nindex=%i\n", index);
 
 }
