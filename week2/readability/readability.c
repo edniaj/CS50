a65d7d33a1990d1ca69365fea6010c0a4e8ee747
jiande <jiande_goh@mymail.sutd.edu.sg>
2022-03-17 03:47:45 +0000

    Thu, Mar 17, 2022, 3:47 AM +00:00

--- week2/readability/readability.c
+++ week2/readability/readability.c
@@ -12,23 +12,23 @@ int main(void)
     word = get_string("Text: ");
     for(int i=0; i<strlen(word); i++)
     {
-        if (strcmp(&word[i]," ") == 0)
+        if (strcmp(word[i]," ") == 0)
         {
             wordCount++;
         }
-        if (isalpha(&word[i]) != 0)
+        if (isalpha(word[i]) != 0)
         {
             charCount++;
         }
-        if (strcmp(&word[i],"?") == 0 )
+        if (strcmp(word[i],"?") == 0 )
         {
             sentence++;
         }
-        if (strcmp(&word[i],".") == 0 )
+        if (strcmp(word[i],".") == 0 )
         {
             sentence++;
         }
-        if (strcmp(&word[i],"!") == 0 )
+        if (strcmp(word[i],"!") == 0 )
         {
             sentence++;
         }
