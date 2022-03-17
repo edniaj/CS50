a4afa0c1353736ae9e71e620b4526c734f030ad0
jiande <jiande_goh@mymail.sutd.edu.sg>
2022-03-17 05:28:06 +0000

    Thu, Mar 17, 2022, 5:28 AM +00:00

--- week2/readability/readability.c
+++ week2/readability/readability.c
@@ -35,17 +35,31 @@ int main(void)
             sentence++;
         }
     }
-    printf("\n %i %i %i",wordCount, charCount, sentence);
+    // printf("\n %i %i %i",wordCount, charCount, sentence);
     float temp = 100 / (float) wordCount;  // 100 / 70 = 1.42 , wordcount = 70
     float L = temp * charCount; // 1.42 * 295 = 421
     float S = temp * sentence; // 1.42 * 3 = 4.2
-    printf("\nL =%f S=%f",L,S);
-    float t = 0.0588 * L; // 24.78
-    printf("\n0.0588 * L = %f\n",t);
-    t = 0.296 * S; // 1.24
-    printf("\n - 0.296 * S  =%f",t);
+    // printf("\nL =%f S=%f",L,S);
+    // float t = 0.0588 * L; // 24.78
+    // printf("\n0.0588 * L = %f\n",t);
+    // t = 0.296 * S; // 1.24
+    // printf("\n - 0.296 * S  =%f",t);
+    // float t = 0.0588 * L - 0.296 * S - 15.8;
+    // printf("%f",t);
     int index = round (0.0588 * L - 0.296 * S - 15.8 ); // 24.78 - 1.24 - 15.8 = 7.7 ~ 8
-    printf("\nindex=%i\n", index);
+    printf("index=%i", index);
+    if (index >=16)
+    {
+        printf('Grade 16+');
+    }
+    if (index <1)
+    {
+        printf('Before Grade 1');
+        }
+    else
+        {
+            printf('Grade %i',index));
+        }
 
 }
 
