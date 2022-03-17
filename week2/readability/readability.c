89a20312e555d5369c525f05688684218730c64f
jiande <jiande_goh@mymail.sutd.edu.sg>
2022-03-17 05:15:42 +0000

    Thu, Mar 17, 2022, 5:15 AM +00:00

--- week2/readability/readability.c
+++ week2/readability/readability.c
@@ -36,13 +36,13 @@ int main(void)
     printf("\n %i %i %i",wordCount, charCount, sentence);
     float temp = 100 / wordCount;  // 100 / 70 = 1.42 , wordcount = 70
     float L = temp * charCount; // 1.42 * 295 = 421
-    float S = temp * sentence; // 1.42 * 
+    float S = temp * sentence; // 1.42 * 3 = 4.2
     printf("\nL =%f S=%f",L,S);
-    float t = 0.0588 * L;
+    float t = 0.0588 * L; // 24.78
     printf("\n0.0588 * L = %f\n",t);
-    t = 0.296 * S;
+    t = 0.296 * S; // 1.24
     printf("\n - 0.296 * S  =%f",t);
-    int index = 0.0588 * L - 0.296 * S - 15.8;
+    int index = 0.0588 * L - 0.296 * S - 15.8; // 24.78 - 1.24 - 15.8 = 7.7 ~ 8
     printf("\nindex=%i\n", index);
 
 }
