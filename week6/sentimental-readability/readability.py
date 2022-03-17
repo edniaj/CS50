6fe62adc952a70191ad43e98b042a86f3294f33d
jiande <jiande_goh@mymail.sutd.edu.sg>
2022-03-17 02:20:35 +0000

    Thu, Mar 17, 2022, 2:20 AM +00:00

--- week6/sentimental-readability/readability.py
+++ week6/sentimental-readability/readability.py
@@ -1,9 +1,12 @@
 def main():
     txt = input("Text: ")
     wordCount = 1
+    character = 0
     for word in txt:
         if word ==  ' ':
             wordCount += 1
-    print(wordCount)
+        if word.isalpha():
+            character +=1
+    print(wordCount,'  ',character)
 
 main()
