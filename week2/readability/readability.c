f29c056f83454c677c990dd406d1382f64d44d62
jiande <jiande_goh@mymail.sutd.edu.sg>
2022-03-17 03:40:48 +0000

    Thu, Mar 17, 2022, 3:40 AM +00:00

--- week2/readability/readability.c
+++ week2/readability/readability.c
@@ -1,15 +1,38 @@
 #include <cs50.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 int main(void)
 {
-    string word;
+    char word[40];
     int wordCount =1;
     int charCount = 0;
     int sentence = 0;
     word = get_string("Text: ");
     for(int i=0; i<strlen(word); i++)
     {
-
+        if (strcmp(word[i]," ") == 0)
+        {
+            wordCount++
+        }
+        if (isalpha(word[i]) != 0)
+        {
+            charCount++
+        }
+        if (strcmp(word[i],"?") == 0 )
+        {
+            sentence++
+        }
+        if (strcmp(word[i],".") == 0 )
+        {
+            sentence++
+        }
+        if (strcmp(word[i],"!") == 0 )
+        {
+            sentence++
+        }
     }
+    printf("\n%i %i %i ",wordCount, charCount, sentence);
 }
+
