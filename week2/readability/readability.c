00f963520adfae70e08f9e14148c920cf24be4c4
jiande <jiande_goh@mymail.sutd.edu.sg>
2022-03-17 03:21:07 +0000

    Thu, Mar 17, 2022, 3:21 AM +00:00

--- week2/readability/readability.c
+++ week2/readability/readability.c
@@ -1,12 +1,12 @@
 #include <cs50.h>
 #include <stdio.h>
-
+#include <stdlib.h>
 int main(void)
 {
-    char* word[40];
+    char* word = malloc(40);
     int wordCount =1;
     int charCount = 0;
     int sentence = 0;
-    fopen()
+    word = get_string("Text: ");
     free(word);
 }
