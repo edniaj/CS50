242415fa118aa6b959d062106068c2f3d283454c
jiande <jiande_goh@mymail.sutd.edu.sg>
2022-03-17 02:24:09 +0000

    Thu, Mar 17, 2022, 2:24 AM +00:00

--- week6/sentimental-readability/readability.py
+++ week6/sentimental-readability/readability.py
@@ -10,3 +10,7 @@ def main():
     print(wordCount,'  ',character)
 
 main()
+
+# Recall that the Coleman-Liau index is computed as 0.0588 * L - 0.296 * S - 15.8,
+# where L is the average number of letters per 100 words in the text,
+# and S is the average number of sentences per 100 words in the text.
