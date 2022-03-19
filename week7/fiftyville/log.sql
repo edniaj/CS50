b6a524b75d94bc60725774dad4ccff34a3874fd5
jiande <jiande_goh@mymail.sutd.edu.sg>
2022-03-19 05:35:52 +0000

    Sat, Mar 19, 2022, 5:35 AM +00:00

--- week7/fiftyville/log.sql
+++ week7/fiftyville/log.sql
@@ -10,11 +10,17 @@ As the thief was leaving the bakery, they called someone who talked to them for 
 that they were planning to take the earliest flight out of Fiftyville tomorrow. The thief then asked the person on the other end of the
 phone to purchase the flight ticket.
 
+
+SELECT *
+FROM atm_transactions
+WHERE day = 28 and atm_location = "Leggett Street"
+2.1 /
+
 2/ I don't know the thief's name, but it was someone I recognized. Earlier this morning, before I arrived at Emma s bakery,
 I was walking by the ATM on Leggett Street and saw the thief there withdrawing some money.
 
 
-1.1/ Potential license plate
+
 5P2BI95
 94KL13X
 6P58WS2
@@ -27,6 +33,8 @@ L93JTIZ
 SELECT *
 FROM bakery_security_logs
 WHERE month = 7 AND day = 28 AND year = 2021 AND hour = 10 AND minute <= 25
+
+1.1/ Potential license plate
 
 1/ Sometime within ten minutes of the theft, I saw the thief get into a car in the bakery parking lot and drive away.
 If you have security footage from the bakery parking lot, you might want to look for cars that left the parking lot in that time frame.
