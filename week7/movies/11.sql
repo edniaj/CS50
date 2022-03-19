9d4f478c86f78c3350c4d1cdbac7fca1faff68d1
jiande <jiande_goh@mymail.sutd.edu.sg>
2022-03-19 04:48:03 +0000

    Sat, Mar 19, 2022, 4:48 AM +00:00

--- week7/movies/11.sql
+++ week7/movies/11.sql
@@ -1,1 +1,13 @@
-wtf is Ma Rainey's black 
+SELECT title
+FROM movies JOIN ratings ON movies.id = ratings.movie_id
+WHERE id in (
+	SELECT movie_id
+	FROM ratings
+	WHERE movie_id in (
+		SELECT movie_id
+		FROM people JOIN stars ON people.id = stars.person_id
+		WHERE people.name = "Chadwick Boseman"
+	)
+)
+ORDER BY ratings.rating DESC
+	LIMIT 5
