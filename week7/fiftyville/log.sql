73567abe4c1b1c34dd9b9a8a743f5599b6560db9
jiande <jiande_goh@mymail.sutd.edu.sg>
2022-03-19 05:23:14 +0000

    Sat, Mar 19, 2022, 5:23 AM +00:00

--- week7/fiftyville/log.sql
+++ week7/fiftyville/log.sql
@@ -1,2 +1,13 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
 
+
+
+
+
+Theft of the CS50 duck took place at 10:15am at the Humphrey Street bakery.
+Interviews were conducted today with three witnesses who were present at the time –
+each of their interview transcripts mentions the bakery.
+
+SELECT *
+FROM crime_scene_reports
+WHERE month = 7 AND day = 28 AND year = 2021
