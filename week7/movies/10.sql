b10857bbf3e983f4287ece7900d245ab53e31d0d
jiande <jiande_goh@mymail.sutd.edu.sg>
2022-03-19 02:55:53 +0000

    Sat, Mar 19, 2022, 2:55 AM +00:00

--- week7/movies/10.sql
+++ week7/movies/10.sql
@@ -1,4 +1,10 @@
-
+SELECT name
+FROM people
+WHERE id in (
+	SELECT directors.person_id
+	FROM directors JOIN ratings on directors.movie_id = ratings.movie_id
+	WHERE ratings.rating >= 9
+)
 
 -- In 10.sql, write a SQL query to list the names of all people who have directed a movie that received a rating of at least 9.0.
 -- Your query should output a table with a single column for the name of each person.
