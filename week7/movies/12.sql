2bf8e0a7c3827274f7c698a73fa7ba1b4a5888c0
jiande <jiande_goh@mymail.sutd.edu.sg>
2022-03-19 04:18:08 +0000

    Sat, Mar 19, 2022, 4:18 AM +00:00

--- week7/movies/12.sql
+++ week7/movies/12.sql
@@ -3,6 +3,10 @@ FROM movies
 WHERE id in (
 	SELECT stars.movie_id
 	FROM stars JOIN people on stars.person_id = people.id
-	WHERE people.name = "Johnny Depp" and people.name = "Helena Bonham Carter"
+	WHERE people.name = "Johnny Depp"
+)  AND id IN  (
+	SELECT stars.movie_id
+	FROM stars JOIN people on stars.person_id = people.id
+	WHERE people.name = "Helena Bonham Carter"
 )
 
