2fe89af504aced61a3eed7c83968f3c69a1c1063
jiande <jiande_goh@mymail.sutd.edu.sg>
2022-03-19 04:31:28 +0000

    Sat, Mar 19, 2022, 4:31 AM +00:00

--- week7/movies/11.sql
+++ week7/movies/11.sql
@@ -11,11 +11,3 @@ WHERE id in (
 	ORDER BY ratings.rating DESC
 	LIMIT 5
 )
-
-
-
-
-
--- write a SQL query to list the titles of the five highest rated movies (in order) that Chadwick Boseman starred in, starting with the highest rated.
--- Your query should output a table with a single column for the title of each movie.
--- You may assume that there is only one person in the database with the name Chadwick Boseman.
