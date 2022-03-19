68a0fe4b0b132618756a11e14c1b4d389fd36bbb
jiande <jiande_goh@mymail.sutd.edu.sg>
2022-03-19 05:29:16 +0000

    Sat, Mar 19, 2022, 5:29 AM +00:00

--- week7/fiftyville/log.sql
+++ week7/fiftyville/log.sql
@@ -1,7 +1,9 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
 
 
-4/ Iu knowm the bakery owner, and someone came in, suspiciously whispering into a phone for about half an hour. They never bought anything.
+
+
+4/ Im the bakery owner, and someone came in, suspiciously whispering into a phone for about half an hour. They never bought anything.
 
 3/
 As the thief was leaving the bakery, they called someone who talked to them for less than a minute. In the call, I heard the thief say
@@ -11,9 +13,13 @@ phone to purchase the flight ticket.
 2/ I don't know the thief's name, but it was someone I recognized. Earlier this morning, before I arrived at Emma s bakery,
 I was walking by the ATM on Leggett Street and saw the thief there withdrawing some money.
 
+
+1.1/ 
 1/ Sometime within ten minutes of the theft, I saw the thief get into a car in the bakery parking lot and drive away.
 If you have security footage from the bakery parking lot, you might want to look for cars that left the parking lot in that time frame.
 
+
+/////////////////////////////////////////////////
 SELECT *
 FROM interviews
 WHERE transcript like "%bakery%"
