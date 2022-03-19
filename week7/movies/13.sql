51e2bc89776cd1143786d9a620a9cafb16c200df
jiande <jiande_goh@mymail.sutd.edu.sg>
2022-03-19 03:39:47 +0000

    Sat, Mar 19, 2022, 3:39 AM +00:00

---
+++ week7/movies/13.sql
@@ -0,0 +1,15 @@
+SELECT person_id
+FROM stars
+WHERE movie_id in (
+	SELECT movie_id
+	FROM stars
+	WHERE person_id = (
+		SELECT id
+		FROM people
+		WHERE name = "Kevin Bacon" AND birth = 1958
+	)
+) AND person_id != (
+	SELECT id
+	FROM people
+	WHERE name = "Kevin Bacon" AND birth = 1958
+)
