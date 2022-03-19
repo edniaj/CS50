99abb3eab8da519946d03b7ce167ef5841e01d5a
jiande <jiande_goh@mymail.sutd.edu.sg>
2022-03-19 03:19:30 +0000

    Sat, Mar 19, 2022, 3:19 AM +00:00

--- week7/movies/11.sql
+++ week7/movies/11.sql
@@ -1,3 +1,17 @@
+SELECT title
+FROM movies
+WHERE id in (
+	SELECT movie_id
+	FROM ratings
+	WHERE movie_id in (
+		SELECT movie_id
+		FROM people JOIN stars ON people.id = stars.person_id
+		WHERE people.name = "Chadwick Boseman"
+	)
+	ORDER BY ratings.rating DESC
+	LIMIT 5
+)
+
 
 
 
