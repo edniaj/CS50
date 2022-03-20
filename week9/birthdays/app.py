23c98da9412b39bb9b68504dd97a1f7771ccfdc9
jiande <jiande_goh@mymail.sutd.edu.sg>
2022-03-20 04:50:18 +0000

    Sun, Mar 20, 2022, 4:50 AM +00:00

--- week9/birthdays/app.py
+++ week9/birthdays/app.py
@@ -35,8 +35,8 @@ def index():
     else:
 
         # TODO: Display the entries in the database on index.html
-
-        return render_template("index.html",months=months, days=days)
+        birthdays = db.execute("SELECT * FROM birthdays")
+        return render_template("index.html",months=months, days=days,birthdays = birthdays)
 
 
 
