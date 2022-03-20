0ac7ff9012adf3bf9ffc7b60dda42c238cca0200
jiande <jiande_goh@mymail.sutd.edu.sg>
2022-03-20 05:04:30 +0000

    Sun, Mar 20, 2022, 5:04 AM +00:00

--- week9/birthdays/app.py
+++ week9/birthdays/app.py
@@ -30,7 +30,7 @@ def index():
         name = request.form.get("name")
         month = request.form.get("month")
         day = request.form.get("day")
-        db.execute("INSERT INTO birthdays (name,month,day) VALUES(?,?,?)",name=name month=month day=day)
+        db.execute("INSERT INTO birthdays (name, month,day) VALUES(?, ?,?)", name, month,day)
         return redirect("/")
 
     else:
