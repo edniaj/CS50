0a4676ad20cc2c574caf52377fe3b7d6a53977bf
jiande <jiande_goh@mymail.sutd.edu.sg>
2022-03-20 04:06:12 +0000

    Sun, Mar 20, 2022, 4:06 AM +00:00

--- week9/birthdays/app.py
+++ week9/birthdays/app.py
@@ -37,3 +37,22 @@ def index():
         return render_template("index.html")
 
 
+
+
+# Complete the implementation of a web application to let users store and keep track of birthdays.
+
+# GET, in a table, all of the people in your database along with their birthdays.
+
+# query the birthdays.db database for all birthdays. Pass all of that data to your index.html template.
+# Then, in index.html, add logic to render each birthday as a row in the table. Each row should have two columns: one column for the person’s name and another column for the person’s birthday.
+# When the / route is requested via POST, your web application should add a new birthday to your database and then re-render the index page.
+# First, in index.html, add an HTML form. The form should let users type in a name, a birthday month, and a birthday day. Be sure the form submits to / (its “action”) with a method of post.
+# Then, in app.py, add logic in your POST request handling to INSERT a new row into the birthdays table based on the data supplied by the user.
+# Optionally, you may also:
+
+# Add the ability to delete and/or edit birthday entries.
+# Add any additional features of your choosing!
+
+
+
+
