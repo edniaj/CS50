7e013fce9a08e9d65bf41ffc1dccda23100eba44
jiande <jiande_goh@mymail.sutd.edu.sg>
2022-03-20 21:18:19 +0000

    Sun, Mar 20, 2022, 9:18 PM +00:00

--- week9/finance/app.py
+++ week9/finance/app.py
@@ -82,7 +82,7 @@ def login():
         rows = db.execute("SELECT * FROM users WHERE username = ?", request.form.get("username"))
 
         # Ensure username exists and password is correct
-        if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
+        if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")): # rows[0] because its the top of the sqlite row
             return apology("invalid username and/or password", 403)
 
         # Remember which user has logged in
