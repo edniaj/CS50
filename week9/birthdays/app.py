bf70a065b0172b05b2f243d25a786b807c7049c9
jiande <jiande_goh@mymail.sutd.edu.sg>
2022-03-20 04:40:03 +0000

    Sun, Mar 20, 2022, 4:40 AM +00:00

--- week9/birthdays/app.py
+++ week9/birthdays/app.py
@@ -12,7 +12,7 @@ app.config["TEMPLATES_AUTO_RELOAD"] = True
 # Configure CS50 Library to use SQLite database
 db = SQL("sqlite:///birthdays.db")
 
-months = [i for i in range(1,13)] #Number of days
+months = [1,2,3] #Number of days
 
 @app.after_request
 def after_request(response):
@@ -36,7 +36,7 @@ def index():
 
         # TODO: Display the entries in the database on index.html
 
-        return render_template("index.html")
+        return render_template("index.html",month=month)
 
 
 
